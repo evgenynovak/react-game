a25c8f4ea5469a9b1c2153bfcdeb6e8a7dcc98d8
evgenynovak <evnovak171@mail.ru>
2021-02-24 21:05:53 +0300

    refactor: add style component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+width: 100%;
+min-height: 100vh;
+padding: 2rem;
+background: #8ac4d0;
+`
 
 function App() {
   return (
-    <div>
+    <AppWrapper>
       123
-    </div>
+    </AppWrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import styled , {createGlobalStyle} from 'styled-components';
 
+const Global = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+}
+`
 
 ReactDOM.render(
-    <App />,
+    <>
+      <Global/>
+      <App />
+    </>,
   document.getElementById('root')
 );
 
